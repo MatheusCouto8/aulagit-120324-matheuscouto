953e0afeeb3681d3e45a03402edd6609983cadae
MatheusSC <matheuscouto.tds2.senai>
2024-03-12 16:33:52 -0300

    removi um comentario

--- aula1-ativ2.js
+++ aula1-ativ2.js
@@ -1,9 +1,5 @@
 let a = 100;
 let b = 60;
 let mensagem = "Resultado:"
-
-// Pegar o let A e fazer subtração com o let B
 let resultado = a - b;
-
-//Mostrara o resultado da operação
 console.log(mensagem + resultado);
